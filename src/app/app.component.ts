87ebd7a8520eab242e156a0fea093844e76cac7a
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-06-12 15:55:13 -0400

    clicking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'teamapp';
+
+  newMember: string = '';
+  members: string[] = [];
+
+  addMember = () => {
+    console.log('CLICKED');
+  };
 }
