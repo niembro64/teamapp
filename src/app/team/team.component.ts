23b165d389b0b6fb8a006887cdcbd6f529e44c21
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-06-12 17:25:24 -0400

    componentized

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent implements OnInit {
+  @Input() team: string[] = [];
+  @Input() index = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
